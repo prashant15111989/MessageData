c8dbca5409471d27dd18d31642de650a1d50c18f
prashantpatvari1598 <prashantpatvari1598@gmail.com>
2019-03-29 01:44:58 +0530

    Add project files.

---
+++ MessageData/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MessageData
+{
+    public class HelloWorld
+    {
+        string mymsg= "HelloWorld";
+        public void show(string msg)
+        {
+            msg = mymsg;
+        }
+    }
+}
